f9c2ad5f45e7728538c1b3632248d99b16fa2ec5
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-11-29 22:29:00 -0300

    start the project

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navbar } from './_components/navbar'
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='h-full bg-slate-100'>
+        {/* Navbar */}
+        <Navbar></Navbar>
+        <main className='pt-40 pb-20 bg-slate-100'>
+            { children }
+        </main>
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default MarketingLayout
